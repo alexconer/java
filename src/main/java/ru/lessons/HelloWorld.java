17b737fcb71f67e48e3b787335e611ab9ebb348d
alexconer <shishkin.com@yandex.ru>
2018-07-24 10:20:53 +0300

    add first app

---
+++ src/main/java/ru/lessons/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.lessons;
+
+/**
+ * Created by ShishkinA on 24.07.2018.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
