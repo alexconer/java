4bb2561544dbd6b2f835c4b79d0d274b412b1278
alexconer <shishkin.com@yandex.ru>
2018-07-31 10:14:35 +0300

    попытка использовать внешнюю библиотеку

--- src/main/java/ru/lessons/inheritance/Main.java
+++ src/main/java/ru/lessons/inheritance/Main.java
@@ -3,6 +3,9 @@ package ru.lessons.inheritance;
 /**
  * Created by ShishkinA on 27.07.2018.
  */
+
+import ru.lessons.utils.Print;
+
 public class Main {
     public static void main(String[] args) {
         User u1 = new User("Alex");
@@ -13,5 +16,7 @@ public class Main {
 
         System.out.println(u1.getSalary());
         System.out.println(u2.getSalary());
+
+        Print.println("Hello");
     }
 }
