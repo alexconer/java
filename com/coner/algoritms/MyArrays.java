72c83c0fb7b19066d2496f278b658f1254b3ac04
alexconer <shishkin.com@yandex.ru>
2019-01-20 19:43:17 +0300

    t

--- com/coner/algoritms/MyArrays.java
+++ com/coner/algoritms/MyArrays.java
@@ -10,6 +10,13 @@ public class MyArrays {
         data = ar;
     }
 
+    public int length(){
+        return data.length;
+    }
+    public int[] getData(){
+        return data;
+    }
+
     public void invert (){
         for (int i = 0; i < data.length/2; i++){
             swap(i, data.length - i - 1);
@@ -38,4 +45,77 @@ public class MyArrays {
     }
 
     //public void sort
+    public void sortBubble(){
+        int step = 0;
+        for (int i = data.length - 1; i >= 0 ; i--){
+            for (int j = i - 1; j >= 0; j--){
+                if (data[j] > data[i]){
+                    swap(i, j);
+                    step ++;
+                }
+            }
+        }
+        System.out.println(step);
+    }
+
+    public void sortSelection(){
+        int step = 0;
+        for (int i = 0; i < data.length - 1; i++) {
+            int k = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[k] > data[j]){
+                    k = j;
+                }
+            }
+            if (k!=i){
+                swap(i, k);
+                step++;
+            }
+        }
+        System.out.println(step);
+    }
+
+    public void sortInsertion(){
+        int step = 0;
+        for (int i = 1; i < data.length; i++){
+            int tmp = data[i];
+            int j = i;
+            while (j > 0 && data[j - 1] >= tmp){
+                data[j] = data[j - 1];
+                j--;
+            }
+            data[j] = tmp;
+            step++;
+        }
+        System.out.println(step);
+    }
+
+    public void fsortInsertion(){
+        int step = 0;
+        for (int i = 1; i < data.length; i++){
+            int tmp = data[i];
+            int pos = binarySearch(tmp,0,i);
+            if (pos < 0) pos = -pos - 1;
+            System.arraycopy(data, pos, data, pos + 1, i -pos);
+            data[pos] = tmp;
+            step++;
+        }
+        System.out.println(step);
+    }
+
+    public int binarySearch(int el, int left, int right){
+        int low = left;
+        int high = right - 1;
+
+        while (high >= low){
+            int mid = low + (high - low)/2;
+            if (data[mid] < el)
+                low = mid + 1;
+            else if (data[mid] > el)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
 }
--- com/coner/procedural/App00.java
+++ com/coner/procedural/App00.java
@@ -2,6 +2,8 @@ package com.coner.procedural;
 
 import com.coner.algoritms.MyArrays;
 
+import java.util.Arrays;
+
 public class App00 {
     public static void main(String[] args) {
         System.out.println("o = " + new Object());
@@ -11,7 +13,35 @@ public class App00 {
         ar.print();
         ar.invertRecursion();
         ar.print();
-
+        System.out.println("sort---->");
         MyArrays ar2 = new MyArrays(new int[]{2,5,3,1,9,6,4,8,7});
+        ar2.sortSelection();
+        ar2.print();
+        System.out.println("sort Insertion---->");
+        MyArrays ar3 = new MyArrays(new int[]{2,5,3,1,9,6,4,8,7});
+        ar3.sortInsertion();
+        ar3.print();
+
+        System.out.println("sort fast Insertion----->");
+        MyArrays ar4 = new MyArrays(new int[]{2,1,5,3,9,6,4,8,7});
+        ar4.fsortInsertion();
+        ar4.print();
+
+        System.out.println("test System.arraycopy----->");
+        int[] a1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] a2 = {2};
+        //System.arraycopy(a2, 0, a1, 4, 5);
+        System.out.println(Arrays.toString(a1));
+        System.out.println(Arrays.toString(a2));
+        int p = Arrays.binarySearch(a2, 1);
+        System.out.println(p);
+
+
+        //System.out.println(ar4.binarySearch(3, 0, ar4.length()));
+        //System.out.println(ar4.binarySearch(6, 0, ar4.length()));
+        //System.out.println(ar4.binarySearch(11, 0, ar4.length()));
+        //System.out.println(Arrays.binarySearch(ar4.getData(),0,1,1));
+        //System.arraycopy(ar4.getData(), 0, ar4.getData(), 1, 1);
+        //ar4.print();
     }
 }
